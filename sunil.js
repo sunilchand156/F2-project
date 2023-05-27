2271917a6cd68c47155b4fcf15e170b2992d9b09
sunilchand156 <122867185+sunilchand156@users.noreply.github.com>
2023-05-27 23:57:34 +0530

    Add files via upload
    first commit

---
+++ sunil.js
@@ -0,0 +1,60 @@
+// var a=10;
+// function foo(){
+// var a=20;
+// console.log(a);
+// console.log(window.a);
+// }
+// console.log(a);
+// window.foo();
+
+// var scope ="js scope";
+// function checkscope()
+// {
+//     var scope="javascript scope";
+// function f()
+// {
+//     return scope;
+// }
+// return f;
+
+// }
+// console.log(scope);
+
+// function tns_outer(){
+//     var a=10;
+//     function tns_inner(){
+//         var b=10;
+//         return a+b;
+//     }
+//     return tns_inner();
+// }
+// console.log(tns_outer());
+
+
+
+
+
+
+
+
+
+
+
+
+
+// {
+//     let message= "hello";
+//     console.log(message);
+// }
+// console.log(message);
+// var x="apple";
+// var y= function(){
+//     if(x=="false"){
+//         console.log("first");
+//     }
+//     if(!x){
+//         console.log("second");
+//     }
+//      var x="banana";
+// };
+// y();
